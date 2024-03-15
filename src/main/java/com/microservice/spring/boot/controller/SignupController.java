8cc23bf6f7149c8cee3dd87b53a4ffff2aaf871e
Shubham Pawar <pawarshubhm79@gmail.com>
2024-03-15 17:29:54 +0530

    try catch and comments

--- src/main/java/com/microservice/spring/boot/controller/SignupController.java
+++ src/main/java/com/microservice/spring/boot/controller/SignupController.java
@@ -1,21 +1,42 @@
 package com.microservice.spring.boot.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import com.microservice.spring.boot.Models.User;
+import com.microservice.spring.boot.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @RestController
 public class SignupController {
     
+    @Autowired 
+    private UserRepository userRepository;
+    
+    // Endpoint to check if the signup service is ready
     @GetMapping("/")
-    public String getMethodName() {
+    public String checkSignupService() {
         return "SignUp Ready";
     }
-/*
-    @GetMapping("/")
-    public String getMethodName() {
-        return "SignUp Ready";
+
+    // Endpoint to handle user signup requests
+    @RequestMapping("/signup-user/{userName}/{password}")
+    public String signUpUser(@PathVariable("userName") String userName, @PathVariable("password") String password) {
+        try {
+            // Create a new User instance
+            User user = new User();
+            // Set username and password
+            user.setUsername(userName);
+            user.setPassword(password);
+            // Save the user to the database
+            userRepository.save(user);
+            // Return success message
+            return "SignUp Completed";
+        } catch (Exception e) {
+            // If an error occurs during signup
+            return "An error occurred during signup: " + e.getMessage();
+        }
     }
-   */  
 }
--- src/main/java/com/microservice/spring/boot/repositories/UserRepository.java
+++ src/main/java/com/microservice/spring/boot/repositories/UserRepository.java
@@ -1,22 +1,9 @@
 package com.microservice.spring.boot.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
 import com.microservice.spring.boot.Models.User;
-import java.util.List;
 
-
-//@EnableJpaRepositories("com.microservice.Repository")
 public interface UserRepository extends JpaRepository<User,Integer>{
-       // Custom query to find a user by username with case sensitivity
-
-    @Query("SELECT u FROM User u WHERE u.username = :username")
-    User findByUsername(@Param("username") String username);
-   
-   // Custom query to find a user by password with case sensitivity
-   @Query("SELECT u FROM User u WHERE u.password = :password")
-    User findByPassword(@Param("password") String password);
+      
     
 }
